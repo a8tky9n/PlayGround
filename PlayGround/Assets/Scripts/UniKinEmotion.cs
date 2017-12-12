b3d56b09edc111faba1707e375b6d425753bf50b
takaya_asano <a8tky9n@gmail.com>
2017-12-12 12:41:14 +0900

    レポート課題の検算用のプロジェクトとソースコードを追加しました

--- PlayGround/Assets/Scripts/UniKinEmotion.cs
+++ PlayGround/Assets/Scripts/UniKinEmotion.cs
@@ -6,6 +6,7 @@ public class UniKinEmotion : MonoBehaviour
 {
     [SerializeField]
     GameObject[] tracker = new GameObject[6];
+    float num;
     // Use this for initialization
     void Start()
     {
@@ -17,17 +18,19 @@ public class UniKinEmotion : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            num = 0;
             for (int i = 0; i < 6; i++)
             {
                 tracker[i].transform.rotation = Random.rotation;
             }
-            DotComp(tracker[0], tracker[3]);
-            DotComp(tracker[1], tracker[4]);
-            DotComp(tracker[2], tracker[5]);
-            PosComp(tracker[0], tracker[1], tracker[3], tracker[4]);
-            PosComp(tracker[0], tracker[2], tracker[3], tracker[5]);
-            RotComp(tracker[0], tracker[1], tracker[3], tracker[4]);
-            RotComp(tracker[0], tracker[2], tracker[3], tracker[5]);
+            num += DotComp(tracker[0], tracker[3]);
+            num += DotComp(tracker[1], tracker[4]);
+            num += DotComp(tracker[2], tracker[5]);
+            num += PosComp(tracker[0], tracker[1], tracker[3], tracker[4]);
+            num += PosComp(tracker[0], tracker[2], tracker[3], tracker[5]);
+            num += RotComp(tracker[0], tracker[1], tracker[3], tracker[4]);
+            num += RotComp(tracker[0], tracker[2], tracker[3], tracker[5]);
+            Debug.Log("総合的に見てこの2つの姿勢の一致度は " + num / 7 + " です。");
         }
     }
     // Y軸を基準にした回転の一致度の比較
@@ -41,22 +44,24 @@ public class UniKinEmotion : MonoBehaviour
         float d2 = Mathf.Clamp(rad2, -1.0f, 1.0f);
         // 内積から角度を計算する
         float theta = Mathf.Acos(d1);
-        Debug.Log(go1.name + " はY軸に対して " + theta + " 度傾いています。");
+        Debug.Log(go1.name + " はY軸に対して " + theta * Mathf.Rad2Deg + " 度傾いています。");
         float theta1 = Mathf.Acos(d2);
-        Debug.Log(go2.name + " はY軸に対して " + theta1 + " 度傾いています。");
+        Debug.Log(go2.name + " はY軸に対して " + theta1 * Mathf.Rad2Deg + " 度傾いています。");
         Vector2 v = new Vector2(Mathf.Cos(theta), Mathf.Sin(theta));
         Vector2 v1 = new Vector2(Mathf.Cos(theta1), Mathf.Sin(theta1));
         float dot = Vector2.Dot(v, v1);
-        Debug.Log(go1.name + " から見た " + go2.name + " は　" + Mathf.Acos(dot) * Mathf.Rad2Deg + " 度傾いていると思われます...");
+        Debug.Log(go1.name + " と " + go2.name + " の一致度は　" + dot + " です...");
         return dot;
     }
     // 座標による一致度の比較
     float PosComp(GameObject goP, GameObject go, GameObject go1P, GameObject go1)
     {
-        Vector3 d = (goP.transform.position - go.transform.position).normalized;
-        Vector3 d1 = (go1P.transform.position - go1.transform.position).normalized;
+        //Vector3 d = (goP.transform.position - go.transform.position).normalized;
+        //Vector3 d1 = (go1P.transform.position - go1.transform.position).normalized;
+        Vector3 d = goP.transform.InverseTransformPoint(go.transform.position).normalized;
+        Vector3 d1 = go1P.transform.InverseTransformPoint(go1.transform.position).normalized;
         float dot = Vector3.Dot(d, d1);
-        Debug.Log(go.name + " と " + go1.name + " は" + Mathf.Acos(dot) * Mathf.Rad2Deg + " 度離れていると思われます...");
+        Debug.Log(go.name + " と " + go1.name + " の一致度は" + dot + " です...");
         return dot;
     }
     // Quaternionを使用した回転の一致度の比較
@@ -67,12 +72,13 @@ public class UniKinEmotion : MonoBehaviour
         // 計算結果をそのままAcosに突っ込むと極稀にNaNを返すのでワンクッション置く
         float d1 = Mathf.Clamp(rad, -1.0f, 1.0f);
         float d2 = Mathf.Clamp(rad1, -1.0f, 1.0f);
+
         float theta = Mathf.Acos(d1);
         float theta1 = Mathf.Acos(d2);
         Vector2 v = new Vector2(Mathf.Cos(theta), Mathf.Sin(theta));
         Vector2 v1 = new Vector2(Mathf.Cos(theta1), Mathf.Sin(theta1));
         float dot = Vector2.Dot(v, v1);
-        Debug.Log(go.name + " から見た " + go1.name + " は　" + Mathf.Acos(dot) * Mathf.Rad2Deg + " 度傾いていると思われます...");
+        Debug.Log(go.name + " と " + go1.name + " の一致度は　" + dot + " です...");
         return dot;
     }
 }
