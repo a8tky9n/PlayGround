9336fc6691acc0cb19cfdbea58b1b01974f138c3
asanoDeskTopPC <asano@a8tky9n.sakura.ne.jp>
2018-01-11 03:39:58 +0900

    no message

---
+++ PlayGround/Assets/Scripts/World2LocalTester.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World2LocalTester : MonoBehaviour {
+    [SerializeField]
+    GameObject Parent;
+    [SerializeField]
+    GameObject Child;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Process();
+        }
+	}
+    void Process()
+    {
+        //Parent.transform.rotation = Random.rotation;
+        //Parent.transform.position = Random.insideUnitSphere;
+        Debug.Log(Parent.transform.worldToLocalMatrix);
+        Debug.Log(Parent.transform.localToWorldMatrix);
+
+    }
+}
